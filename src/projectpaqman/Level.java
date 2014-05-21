f9004435e8936466acce2b4a9d1e1454548955bb
Jerrold <Jerrold@Jerrold-PC>
2014-05-21 23:38:43 +0200

    Vakjes worden aangemaakt met spelelementen

--- src/projectpaqman/Level.java
+++ src/projectpaqman/Level.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package projectpaqman;
 
 import java.awt.*;
@@ -14,53 +13,67 @@ import javax.swing.JPanel;
  *
  * @author Jerrold
  */
-public class Level extends JPanel{
+public class Level extends JPanel {
 
-    public Level(){
-        
+    public Level() {
+
     }
-    
+
     private String naam;
     private int lengte;
     private int breedte;
     private Vakje[][] vakjes;
     private String[][] layout = {
-                                    {"m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "s", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
-                                    {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"}
-                                  };
-    
-    public Level(String naam, int lengte, int breedte){
+        {"m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "s", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"},
+        {"m", "p", "x", "x", "x", "x", "m", "b", "x", "x", "x", "m", "m", "m", "x", "x", "x", "x", "x", "m"}
+    };
+
+    public Level(String naam, int breedte, int lengte) {
         this.setBackground(Color.WHITE);
         this.naam = naam;
         this.lengte = lengte;
         this.breedte = breedte;
-        this.vakjes = new Vakje[lengte/30][breedte/30];
-        
-        for(int x = 0; x < vakjes.length; x++){
-            for(int y = 0; y < vakjes[x].length; y++){
-                vakjes[x][y] = new Vakje(true, x*30, y*30);
-            }
-        }
+        this.vakjes = new Vakje[breedte / 30][lengte / 30];
+
+//        for (int x = 0; x < vakjes.length; x++) {
+//            for (int y = 0; y < vakjes[x].length; y++) {
+//                vakjes[x][y] = new Vakje(true, x * 30, y * 30);
+//            }
+//        }
     }
 
     @Override
-    public void paintComponent(Graphics g){
-        for(int x=0; x < vakjes.length; x++) {
-            for(int y=0; y < vakjes[x].length; y++) {
-                vakjes[x][y].draw(g);
+    public void paintComponent(Graphics g) {
+        for (int x = 0; x < vakjes.length; x++) {
+            for (int y = 0; y < vakjes[x].length; y++) {
+                System.out.println(x + " " + y);
+                switch (layout[y][x]) {
+                    case ("m"):
+                        vakjes[x][y] = new Vakje(g, x * 30, y * 30, Vakje.inhoudVakje.muur);
+                        break;
+                    case ("b"):
+                        vakjes[x][y] = new Vakje(g, x * 30, y * 30, Vakje.inhoudVakje.bolletje);
+                        break;
+                    case ("s"):
+                        vakjes[x][y] = new Vakje(g, x * 30, y * 30, Vakje.inhoudVakje.superbolletje);
+                        break;
+                    default:
+                        vakjes[x][y] = new Vakje(g, x * 30, y * 30, Vakje.inhoudVakje.leeg);
+                        break;
+                }
             }
         }
-        
+
 //        for(int x=0; x < layout.length; x++){
 //            for(int y=0; y < layout[x].length; y++){
 //                switch(layout[x][y]){
--- src/projectpaqman/Vakje.java
+++ src/projectpaqman/Vakje.java
@@ -20,18 +20,44 @@ public class Vakje {
     private int posY;
     private Spelelement elt;
     
-    public Vakje(boolean muur, int posX, int posY) {
-        this.muur = muur;
-        this.posX = posX;
-        this.posY = posY;
+    public enum inhoudVakje{
+        bolletje,
+        superbolletje,
+        muur,
+        leeg
     }
     
-    public void draw(Graphics g) {
+    public Vakje(Graphics g, int posX, int posY, inhoudVakje inhoudVakje) {
+        this.posX = posX;
+        this.posY = posY;
+        draw(g, inhoudVakje);
+    }
+    
+
+    
+    public void draw(Graphics g, inhoudVakje inhoudVakje) {
         g.setColor(Color.BLACK);
-        g.drawRect(posX, posY, 30, 30);
-        if (elt!=null){
-            //elt.draw(x,y,g);
+        switch (inhoudVakje){
+            case bolletje:
+                g.drawRect(posX, posY, 30, 30);
+                Bolletje b = new Bolletje();
+                b.draw(g, posX, posY);
+                break;
+            case superbolletje:
+                g.drawRect(posX, posY, 30, 30);
+                Superbolletje s = new Superbolletje();
+                s.draw(g, posX, posY);
+                break;
+            case muur:
+                g.drawRect(posX, posY, 30, 30);
+                g.fillRect(posX, posX, 30, 30);
+                break;
+            case leeg:
+                g.drawRect(posX, posY, 30, 30);
+                break;
+                
         }
+
     }
     
     public int getPosX(){
